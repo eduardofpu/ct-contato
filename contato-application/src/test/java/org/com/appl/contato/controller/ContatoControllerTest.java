95c4985777be854ea61c0d0806778bab1b51d41a
Eduardo Rodrigues Delfino <eduardo27_minotauro@hotmail.com>
2020-11-04 09:28:22 -0300

    Update ContatoControllerTest.java

--- contato-application/src/test/java/org/com/appl/contato/controller/ContatoControllerTest.java
+++ contato-application/src/test/java/org/com/appl/contato/controller/ContatoControllerTest.java
@@ -118,8 +118,7 @@ public class ContatoControllerTest {
 
         this.mockMvc.perform(MockMvcRequestBuilders.get(LIST).accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())                
                 .andExpect(jsonPath("$.content").exists())
                 .andExpect(jsonPath("$.content").isEmpty())
                 .andExpect(jsonPath("$.totalElements").value(0));
@@ -130,8 +129,7 @@ public class ContatoControllerTest {
 
         when(mockService.findall(any())).thenReturn(listPage);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get(LIST).accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
+        this.mockMvc.perform(MockMvcRequestBuilders.get(LIST).accept(MediaType.APPLICATION_JSON))                
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("$.totalElements").value(2));
